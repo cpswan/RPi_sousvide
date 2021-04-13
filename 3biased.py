bd2ed69b73af612a9d8ed0309754f96a4b875337
Chris Swan <478926+cpswan@users.noreply.github.com>
2021-04-13 19:22:22 +0000

    Control script updated to Python3

--- 3biased.py
+++ 3biased.py
@@ -1,16 +1,26 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3.9
 import os
 from subprocess import Popen, PIPE, call
 from optparse import OptionParser
 from time import sleep
 
-def tempdata():
+def read_temp_raw():
     # Replace 28-000004ce4a45 with the address of your DS18B20
-    pipe = Popen(["cat","/sys/bus/w1/devices/w1_bus_master1/28-000004ce4a45/w1_slave"], stdout=PIPE)
-    result = pipe.communicate()[0]
-    result_list = result.split("=")
-    temp_mC = int(result_list[-1]) # temp in milliCelcius
-    return temp_mC
+    sensor = '/sys/bus/w1/devices/w1_bus_master1/28-000004ce4a45/w1_slave'
+    sensor_input = open(sensor, 'r')
+    sensor_lines = sensor_input.readlines()
+    sensor_input.close()
+    return sensor_lines
+ 
+def tempdata():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        return int(temp_string)
 
 def setup_1wire():
   os.system("sudo modprobe w1-gpio && sudo modprobe w1-therm")
@@ -32,7 +42,7 @@ parser.add_option("-i", "--integral", type = int, default = 2)
 parser.add_option("-b", "--bias", type = int, default = 22)
 (options, args) = parser.parse_args()
 target = options.target * 1000
-print ('Target temp is %d' % (options.target))
+print(('Target temp is %d' % (options.target)))
 P = options.prop
 I = options.integral
 B = options.bias
@@ -62,12 +72,12 @@ while True:
     error = target - temperature
     interror = interror + error
     power = B + ((P * error) + ((I * interror)/100))/100
-    print power
+    print(power)
     if (power > 0):
         pwr_tot = pwr_tot + power
     pwr_ave = pwr_tot / pwr_cnt
     pwr_cnt = pwr_cnt + 1
-    print pwr_ave
+    print(pwr_ave)
     # Make sure that if we should be off then we are
     if (state=="off"):
         turn_off()
